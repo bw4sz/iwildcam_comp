e17c8cb21dc41ad4d2e6d539a61a8f82a7e27d87
bw4sz <benweinstein2010@gmail.com>
2019-04-23 19:09:38 -0700

    resnet

---
+++ DeepTrap/models/resnet.py
@@ -0,0 +1,63 @@
+#Deep Residual Network
+import keras_resnet.models
+import keras
+from keras.utils import get_file
+
+from .. import utils
+
+
+class Model():
+    
+    def __init__(self, config):
+        self.config = config
+        self.image_size = config["image_size"]
+        shape, classes = (self.image_size , self.image_size , 3), len(utils.classes)
+        
+        #Define input shape
+        x = keras.layers.Input(shape)
+        self.model = keras_resnet.models.ResNet50(x, classes=classes)
+        self.model.compile("adam", "categorical_crossentropy", ["accuracy"])
+        
+        #Load imagenet weights
+        imagenet_weights = self.download_imagenet()
+        self.model.load_weights(imagenet_weights, by_name=True, skip_mismatch=True)
+    
+    def download_imagenet(self):
+        """ Downloads ImageNet weights and returns path to weights file.
+        From https://github.com/fizyr/keras-retinanet/blob/9f6ee78a67d27e5d6d9055c2a0da3803e03b290b/keras_retinanet/models/resnet.py
+        """
+        resnet_filename = 'ResNet-{}-model.keras.h5'
+        resnet_resource = 'https://github.com/fizyr/keras-models/releases/download/v0.0.1/{}'.format(resnet_filename)
+        #depth = int(self.backbone.replace('resnet', '')) #TODO allow different depths?
+        depth=50
+
+        filename = resnet_filename.format(depth)
+        resource = resnet_resource.format(depth)
+        if depth == 50:
+            checksum = '3e9f4e4f77bbe2c9bec13b53ee1c2319'
+        elif depth == 101:
+            checksum = '05dc86924389e5b401a9ea0348a3213c'
+        elif depth == 152:
+            checksum = '6ee11ef2b135592f8031058820bb9e71'
+
+        return get_file(
+            filename,
+            resource,
+            cache_subdir='models',
+            md5_hash=checksum
+        )
+    
+    def train(self, train_generator, callbacks=None):
+        
+        self.model.fit_generator(
+            generator=train_generator,
+            steps_per_epoch=train_generator.size()/self.config["batch_size"],
+            epochs=self.config["epochs"],
+            verbose=2,
+            shuffle=False,
+            callbacks=callbacks,
+        )        
+            
+    def predict(self, generator):
+        predictions = self.model.predict_generator(generator)
+        return predictions
---
+++ DeepTrap/preprocess.py
@@ -0,0 +1,49 @@
+#preprocess images
+import numpy as np
+import cv2
+
+
+def resize_image(img, size=299):
+    """ Resize an image such that the size is constrained to min_side and max_side.
+
+    Args
+        min_side: The image's min side will be equal to min_side after resizing.
+        max_side: If after resizing the image's max side is above max_side, resize until the max side is equal to max_side.
+
+    Returns
+        A resized image.
+    """
+    # compute scale to resize the image
+    # resize the image with the computed scale
+    img = cv2.resize(img, (size,size))
+
+    return img
+
+def preprocess_image(x, mode='caffe'):
+    """ Preprocess an image by subtracting the ImageNet mean.
+
+    Args
+        x: np.array of shape (None, None, 3) or (3, None, None).
+        mode: One of "caffe" or "tf".
+            - caffe: will zero-center each color channel with
+                respect to the ImageNet dataset, without scaling.
+            - tf: will scale pixels between -1 and 1, sample-wise.
+
+    Returns
+        The input with the ImageNet mean subtracted.
+    """
+    # mostly identical to "https://github.com/keras-team/keras-applications/blob/master/keras_applications/imagenet_utils.py"
+    # except for converting RGB -> BGR since we assume BGR already
+
+    # covert always to float32 to keep compatibility with opencv
+    x = x.astype(np.float32)
+
+    if mode == 'tf':
+        x /= 127.5
+        x -= 1.
+    elif mode == 'caffe':
+        x[..., 0] -= 103.939
+        x[..., 1] -= 116.779
+        x[..., 2] -= 123.68
+
+    return x
