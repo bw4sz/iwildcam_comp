4301049f13fb9c4dcae088c6be6ccc19ba29270e
bw4sz <benweinstein2010@gmail.com>
2019-04-30 13:00:10 -0700

    restructuring location.py for dask

--- DeepTrap/Locations.py
+++ DeepTrap/Locations.py
@@ -9,36 +9,16 @@ import utils
 import Detector
 import create_h5
 
-#Read and log config file
-config = utils.read_config(prepend="..")
-output_dir = config["output_dir"]
-
-debug=True
-#check for image dir
-#if not os.path.exists(output_dir):
-    #os.mkdir(output)
+def preprocess_location(location_data):
+    """A dictionary object with keys day and night split by pandas image data"""
     
-#use local image copy
-if debug:
-    config["train_data_path"] = "../tests/data/sample_location"
-    config["h5_dir"] = "/Users/Ben/Downloads/"
-
-#Load data
-train_df = pd.read_csv('../data/train.csv')
-train_df['file_path'] = train_df['id'].apply(lambda x: os.path.join(config["train_data_path"], f'{x}.jpg'))
-train_df = utils.check_images(train_df, config["train_data_path"])
-
-#Sort images into location
-locations  = BackgroundSubtraction.sort_locations(train_df)
-
-for location in locations:
     location_images = []    
     location_labels = []
     location_filenames = []
-    for day_or_night in locations[location]:
-            
+    
+    for day_or_night in location_data:
         #Selection image data
-        image_data = locations[location][day_or_night]
+        image_data = location_data[day_or_night]
         
         #sort by timestamp
         image_data = image_data.sort_values("date_captured")
@@ -55,13 +35,34 @@ for location in locations:
         location_filenames.append(filenames)
     
     #Write h5 file
-    create_h5.generate(location_images, location_labels, location_filenames, destination_dir, location)
+    #Flatten out lists
+    location_images = [item for sublist in location_images for item in sublist]
+    location_labels = [item for sublist in location_labels for item in sublist]
+    location_filenames = [item for sublist in location_filenames for item in sublist]
     
-    
-    
-    
+    create_h5.generate(location_images, location_labels, location_filenames, destination_dir= config["h5_dir"], location=location)
 
+if __name__=="__main__":
+    #Read and log config file
+    config = utils.read_config(prepend="..")
+    debug=True
+    
+    #use local image copy
+    if debug:
+        config["train_data_path"] = "../tests/data/sample_location"
+        config["h5_dir"] = "/Users/Ben/Downloads/"
+    
+        #check for image dir
+        #if not os.path.exists(output_dir):
+            #os.mkdir(output)
+    #Load data
+    train_df = pd.read_csv('../data/train.csv')
+    train_df['file_path'] = train_df['id'].apply(lambda x: os.path.join(config["train_data_path"], f'{x}.jpg'))
+    train_df = utils.check_images(train_df, config["train_data_path"])
+    
+    #Sort images into location
+    locations  = BackgroundSubtraction.sort_locations(train_df)
         
-#Save predicted empty based on temporal median
-#Convert to dataframe and save
-#predicted_empty
+    for location in locations:
+        location_data = locations[location]
+        preprocess_location(location_data)    
