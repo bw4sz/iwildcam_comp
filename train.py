50a2e32c95a420f863c808c89885f8bed001e439
bw4sz <benweinstein2010@gmail.com>
2019-05-01 11:39:27 -0700

    better error handling

--- train.py
+++ train.py
@@ -44,6 +44,8 @@ train_df = utils.read_train_data(image_dir=config["train_data_path"], supp_data=
 #Ensure images exist
 train_df = utils.check_images(train_df, config["train_data_path"])
 
+#TODO check h5 preprocessed utils.check_h5
+
 #Create keras training generator - split the training data into a validation set, both from the California site.
 training_split, evaluation_split = utils.split_training(train_df, image_dir=config["train_data_path"] )
 
