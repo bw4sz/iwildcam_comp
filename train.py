0537d22fca56a6aa90a007de4e7e139078761a06
bw4sz <benweinstein2010@gmail.com>
2019-05-03 16:08:24 -0700

    building slow

--- train.py
+++ train.py
@@ -58,9 +58,9 @@ training_split, evaluation_split = utils.split_training(train_df, image_dir=conf
 
 #remove empty from set for testing.
 #Try to minimize sources of risk here, just take 100 images from both
-#if not mode.debug:
-    #training_split = training_split[training_split.category_id.isin([0,1])].groupby("category_id",as_index=False).apply(lambda x: x.head(100))
-    #evaluation_split = evaluation_split[evaluation_split.category_id.isin([0,1])]
+if not mode.debug:
+    training_split = training_split[training_split.category_id.isin([0,1])].groupby("category_id",as_index=False).apply(lambda x: x.head(400))
+    evaluation_split = evaluation_split[evaluation_split.category_id.isin([0,1])]
 
 experiment.log_parameter("Training Images", training_split.shape[0])
 
